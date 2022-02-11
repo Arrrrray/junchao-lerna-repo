a77daeb8b1267d80bd9b2fdb2e01ffd5df7a7773
Gao Junchao <junchao@umu.com>
2022-02-11 21:16:16 +0800

    add LICENSE.md

--- packages/watermark/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { renderDoubleWatermark, IRenderDoubleWatermarkProps } from './renderDoubleWatermark';
-import { renderSingleWatermark, IRenderSingleWatermarkProps } from './renderSingleWatermark';
-export { renderDoubleWatermark, IRenderDoubleWatermarkProps, renderSingleWatermark, IRenderSingleWatermarkProps };
