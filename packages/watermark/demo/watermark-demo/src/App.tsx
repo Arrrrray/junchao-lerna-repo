c2dcab49c5a52bfc7e748f775b65af120e6f91e0
Gao Junchao <junchao@umu.com>
2022-02-11 20:48:57 +0800

    watermark 双绘制，完成第一版

--- packages/watermark/demo/watermark-demo/src/App.tsx
+++ packages/watermark/demo/watermark-demo/src/App.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { renderWatermark } from "../../../src/index";
-
 function App() {
-    renderWatermark({ text: "shuiyin" });
     return (
         <div className="App">
             <header className="App-header">
--- packages/watermark/src/renderWatermark.ts
+++ packages/watermark/src/renderWatermark.ts
@@ -16,7 +16,7 @@ export interface WatermarkProps {
   */
     size?: number;
     /**
-      * 旋转角度
+      * 旋转角度 0～75
     */
     rotate?: number;
     /**
@@ -28,10 +28,30 @@ export interface WatermarkProps {
     */
     transparency?: number;
     /**
+      * x方向偏移
+    */
+    offsetX?: number;
+    /**
+      * y方向偏移
+    */
+    offsetY?: number;
+    /**
       * 水印层级
     */
     zIndex?: number;
 
+}
+export interface IRenderBaseImageProps {
+    width: number;
+    height: number
+    text: string;
+    color: string;
+    fontSize: number;
+    x: number;
+    y: number;
+    rotate: number;
+    font: string;
+    alpha: number;
 }
 
 // 获取屏幕像素比
@@ -69,39 +89,74 @@ const scaleOptions = function (ratio: number, options: Record<string, any>) {
     return newOptions;
 };
 
-const createImageBase = (props: WatermarkProps) => {
-    const { className = 'body', text = '水印文案', color = '#222', size = 13, rotate = 10, font = 'sans-serif', transparency = 0.5, zIndex = 1000 } = props;
+const createImageBase = (props: IRenderBaseImageProps) => {
+    const { width, height, text, color, fontSize, x, y, rotate, font, alpha } = props;
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
-    const ratio = getPixelRatio(ctx);
-    const newOptions = scaleOptions(ratio, { size, rotate, font, transparency, zIndex });
-    const { size: fontSize, offsetX, offsetY, mal_offset, transparency: alpha, zIndex: zIndexNum } = newOptions;
     if (ctx) {
-
-        const textWidth = ctx.measureText(text).width;
-        const currentWidth = textWidth * ratio;
-        const currentHeight = textWidth * ratio;
-        canvas.width = currentWidth;
-        canvas.height = currentHeight;
         ctx.font = `${fontSize}px ${font}`;
         ctx.fillStyle = color;
         ctx.globalAlpha = alpha;
         ctx.textAlign = 'left';
         ctx.textBaseline = 'middle';
-        ctx.translate(currentWidth / 2, currentHeight / 2);
+        ctx.translate(0, 0);
+        canvas.width = width;
+        canvas.height = height;
         ctx.rotate(-rotate * Math.PI / 180);
+        ctx.fillText(text, x, y);
         return canvas.toDataURL('image/png');
     }
 }
 
 export const renderWatermark = (props: WatermarkProps) => {
-    const { className = 'body', text = '水印文案', color = '#222', size = 13, rotate = 10, font = 'sans-serif', transparency = 0.5, zIndex = 1000 } = props;
-    const top = 0;
-    const left = 0;
-    const dataUrl = createImageBase(props);
-    let defaultStyle = document.createElement('style');
-    defaultStyle.innerHTML = `${className} :after {
-        content: '';
+    const { className = 'body', text = '水印文案', color = '#222', size = 13, rotate = 10, offsetX = 15, offsetY = 30, font = 'sans-serif', transparency = 0.5, zIndex = 1000 } = props;
+    // 创建一个canvas，获取文案的宽度，计算出需要设置的canvas的宽高
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    // 获取屏幕像素比
+    const ratio = getPixelRatio(ctx);
+    const newOptions = scaleOptions(ratio, { size, rotate, font, offsetX, offsetY, transparency, zIndex });
+    const { size: fontSize, offsetX: x, offsetY: y, mal_offset, transparency: alpha, zIndex: zIndexNum } = newOptions;
+    if (ctx) {
+        console.log('newOptions: ', newOptions)
+        ctx.font = `${fontSize}px ${font}`;
+        ctx.translate(0, 0);
+        // 获取文案宽高
+        const textWidth = Math.ceil(ctx.measureText(text).width);
+
+        // 计算出需要设置的canvas的宽高，加fontSize作为留白
+        const currentWidth = Math.ceil(textWidth * Math.cos(rotate * Math.PI / 180)) / ratio + fontSize;
+        const currentHeight = Math.ceil(textWidth * Math.sin(rotate * Math.PI / 180)) / ratio + fontSize;
+        const top = 0;
+        const left = 0;
+        const position = 'absolute';
+        const dataUrl1 = createImageBase({
+            width: currentWidth,
+            height: currentHeight * 2,
+            text,
+            color,
+            fontSize,
+            x: -(Math.ceil(currentHeight * Math.sin(rotate * Math.PI / 180)) - fontSize),
+            y: Math.ceil(currentHeight * Math.cos(rotate * Math.PI / 180)),
+            rotate,
+            font,
+            alpha,
+        });
+        const dataUrl2 = createImageBase({
+            width: currentWidth,
+            height: currentHeight * 2,
+            text,
+            color,
+            fontSize,
+            x: -(Math.ceil(currentHeight * Math.sin(rotate * Math.PI / 180)) - fontSize) + x,
+            y: Math.ceil(currentHeight * Math.cos(rotate * Math.PI / 180)) + y,
+            rotate,
+            font,
+            alpha,
+        });
+        let defaultStyle = document.createElement('style');
+        defaultStyle.innerHTML = `${className}:after {
+      content: '';
       display: block;
       width: 100%;
       height: 100%;
@@ -110,5 +165,15 @@ export const renderWatermark = (props: WatermarkProps) => {
       background-repeat: repeat;
       pointer-events: none;
     }`;
-    document.head.appendChild(defaultStyle);
+
+        let styleEl = document.createElement('style');
+        styleEl.innerHTML = `${className}:after
+    {
+      ${position ? `position: ${position}` : ''};
+      ${zIndex ? `z-index:${zIndex}` : ''};
+      background-image: url(${dataUrl1}), url(${dataUrl2});
+    }`;
+        document.head.appendChild(defaultStyle);
+        document.head.appendChild(styleEl);
+    }
 }
